6d2212abbd5704216b6a4a1045a2ba738c1c33b5
Nathan Parry <nparry@gmail.com>
2010-02-21 15:32:02 -0500

    Some comments

--- src/main/scala/com/nparry/orderly/OrderlyParser.scala
+++ src/main/scala/com/nparry/orderly/OrderlyParser.scala
@@ -1,3 +1,35 @@
+/*
+ * Copyright (c) 2010, Nathan Parry
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * 
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ * 
+ *  3. Neither the name of Nathan Parry nor the names of any
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ *  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ *  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.nparry.orderly
 
 import scala.util.parsing.combinator._
@@ -9,16 +41,25 @@ import net.liftweb.json.JsonAST._
 import net.liftweb.json.Implicits._
 import java.io.{InputStream, InputStreamReader}
 
+/**
+ * An implementation of Orderly JSON (http://orderly-json.org/).
+ * This parser produces a JSON object representing the JSON
+ * schema of the given Orderly input.
+ *
+ * This is based on the Orderly grammar at:
+ * http://github.com/lloyd/orderly/blob/master/docs.md
+ */
 object OrderlyParser extends JavaTokenParsers {
   type Tokens = json.Lexer
-  //val lexical = new Tokens
-        
-  //lexical.reserved ++= List("true", "false", "null")
-  //lexical.delimiters ++= List("{", "}", "[", "]", ":", ",")
+
+  // Some helpers to shorten the code below
 
   def t(name: String) = f("type", name)
   def f(k: String, v: JValue) = JField(k, v)
   def l [A] (x: Option[List[A]]): List[A] = x getOrElse List()
+
+
+  // The orderly grammar
 
   def orderlySchema: Parser[JObject] = unnamedEntry <~ ";"
   def namedEntries: Parser[JObject]   = repsep(namedEntry, ";") ^^ (JObject(_))
@@ -74,6 +115,9 @@ object OrderlyParser extends JavaTokenParsers {
     "[A-Za-z_-]+".r ^^ { JString(_) }
   def perlRegex: Parser[List[JField]] = ("/" ~> "[^/]".r <~ "/") ^^
     { case r => List(f("pattern", r)) }
+
+
+  // Mini grammar to parse JSON
 
   def jsonObj: Parser[JObject] = "{" ~> repsep(jsonMember, ",") <~ "}" ^^ { JObject(_) }
   def jsonMember = stringLiteral ~ ":" ~ jsonValue ^^ { case k ~ ":" ~ v => f(k, v) }
