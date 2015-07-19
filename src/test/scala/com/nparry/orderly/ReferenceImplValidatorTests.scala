e7e05ff9b8a6a6513bc89d5523d4aa7a5eb8660c
Nathan Parry <nparry@gmail.com>
2015-07-19 17:16:39 -0400

    Remove trailing whitespace

--- src/test/scala/com/nparry/orderly/ReferenceImplValidatorTests.scala
+++ src/test/scala/com/nparry/orderly/ReferenceImplValidatorTests.scala
@@ -5,19 +5,19 @@
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are
  *  met:
- * 
+ *
  *  1. Redistributions of source code must retain the above copyright
  *     notice, this list of conditions and the following disclaimer.
- * 
+ *
  *  2. Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer in
  *     the documentation and/or other materials provided with the
  *     distribution.
- * 
+ *
  *  3. Neither the name of Nathan Parry nor the names of any
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -111,13 +111,13 @@ class ReferenceImplValidatorTests extends Specification {
           case e:Exception => throw e
         }
       }
-    ) 
+    )
   }
 
   def makeOrderly(f: File): Orderly = try {
     Orderly(f)
   } catch {
-    case e:InvalidOrderly => try { 
+    case e:InvalidOrderly => try {
       // Some of the test input is actually in json schema format
       import net.liftweb.json.JsonAST.JObject
       Json.parse(f) match {
--- src/test/scala/com/nparry/orderly/SimpleSchemaValidationTests.scala
+++ src/test/scala/com/nparry/orderly/SimpleSchemaValidationTests.scala
@@ -5,19 +5,19 @@
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are
  *  met:
- * 
+ *
  *  1. Redistributions of source code must retain the above copyright
  *     notice, this list of conditions and the following disclaimer.
- * 
+ *
  *  2. Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer in
  *     the documentation and/or other materials provided with the
  *     distribution.
- * 
+ *
  *  3. Neither the name of Nathan Parry nor the names of any
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -41,77 +41,77 @@ class SimpleSchemaValidationTests extends Specification {
   "Schema validation" should {
     "do integer validation" in {
       val o = Orderly("integer;")
-  
+
       o.validate(JInt(30)).size mustEqual 0
-  
+
       o.validate(JString("foo")).size mustEqual 1
       o.validate(JDouble(34.3)).size mustEqual 1
       o.validate(JBool(true)).size mustEqual 1
       o.validate(Json.parse("""{ "foo": "bar" }""")).size mustEqual 1
       o.validate(Json.parse("""[ "foo", "bar" ]""")).size mustEqual 1
     }
-  
+
     "do string validation" in {
       val o = Orderly("string;")
-  
+
       o.validate(JString("foo")).size mustEqual (0)
-  
+
       o.validate(JInt(30)).size mustEqual 1
       o.validate(JDouble(34.3)).size mustEqual 1
       o.validate(JBool(true)).size mustEqual 1
       o.validate(Json.parse("""{ "foo": "bar" }""")).size mustEqual 1
       o.validate(Json.parse("""[ "foo", "bar" ]""")).size mustEqual 1
     }
-  
+
     "do string validation with strings containing newlines" in {
       val o = Orderly("string;")
       o.validate(JString("foo\n")).size mustEqual (0)
     }
-  
+
     "do number validation" in {
       val o = Orderly("number;")
-  
+
       o.validate(JDouble(34.3)).size mustEqual 0
-  
+
       // We say an int is a number - this is probably
       // the right thing to do
       o.validate(JInt(30)).size mustEqual 0
-  
+
       o.validate(JString("foo")).size mustEqual 1
       o.validate(JBool(true)).size mustEqual 1
       o.validate(Json.parse("""{ "foo": "bar" }""")).size mustEqual 1
       o.validate(Json.parse("""[ "foo", "bar" ]""")).size mustEqual 1
     }
-  
+
     "do boolean validation" in {
       val o = Orderly("boolean;")
-  
+
       o.validate(JBool(true)).size mustEqual 0
-  
+
       o.validate(JInt(30)).size mustEqual 1
       o.validate(JString("foo")).size mustEqual 1
       o.validate(JDouble(34.3)).size mustEqual 1
       o.validate(Json.parse("""{ "foo": "bar" }""")).size mustEqual 1
       o.validate(Json.parse("""[ "foo", "bar" ]""")).size mustEqual 1
     }
-  
+
     "do object validation" in {
       val o = Orderly("object { string foo; };")
-      
+
       o.validate(Json.parse("""{ "foo": "bar" }""")).size mustEqual 0
-  
+
       o.validate(JInt(30)).size mustEqual 1
       o.validate(JString("foo")).size mustEqual 1
       o.validate(JDouble(34.3)).size mustEqual 1
       o.validate(JBool(true)).size mustEqual 1
       o.validate(Json.parse("""[ "foo", "bar" ]""")).size mustEqual 1
     }
-  
+
     "do array validation" in {
       val o = Orderly("""array [ string ];""")
-      
+
       o.validate(Json.parse("""[ "foo", "bar" ]""")).size mustEqual 0
-  
+
       o.validate(JInt(30)).size mustEqual 1
       o.validate(JString("foo")).size mustEqual 1
       o.validate(JDouble(34.3)).size mustEqual 1
